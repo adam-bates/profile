b7f3e1d8ecfa71c757f43eb735f041dd5d2be491
Adam Bates <adam@adambates.ca>
2018-05-12 18:36:13 -0400

    Added empty components to make up profile page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { CapabilitiesComponent } from './capabilities/capabilities.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    CapabilitiesComponent,
+    ExperienceComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
